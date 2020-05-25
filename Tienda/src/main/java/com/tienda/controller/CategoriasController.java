85d59b64a96ac53e3f5fca03ad025b21751795b8
Carlos Javier Pita Amaya <javierpita17@hotmail.com>
2020-05-25 17:03:53 -0500

    Se realiza el Controlador de la clase Categoria

--- Tienda/src/main/java/com/tienda/controller/CategoriasController.java
+++ Tienda/src/main/java/com/tienda/controller/CategoriasController.java
@@ -1,8 +1,26 @@
 package com.tienda.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tienda.model.Categoria;
+import com.tienda.service.ICategoriaService;
 
 @Controller
+@RequestMapping(value = "/categorias")
 public class CategoriasController {
+	
+	@Autowired
+	private ICategoriaService serviceCategoria;
 
+	public String mostrarIndex(Model model) {
+		List<Categoria> lista = serviceCategoria.buscarTodas();
+		model.addAttribute("categorias",lista);
+		return "categorias/listaCategorias";
+	}
+	
 }
